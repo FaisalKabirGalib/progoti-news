ad2446b591a78c58f174b5c4bd85aee981ffb6b1
Faisal Kabir Galib <faisal@dana.money>
2023-03-04 17:42:51 +0600

    add login pages

--- src/pages/AddNews.page.tsx
+++ src/pages/AddNews.page.tsx
@@ -1,9 +1,12 @@
-import { FC, useState } from 'react';
+import { FC, useEffect, useState } from 'react';
 
 import AddPost from '../components/Form/AddPost';
 import AddCategories from '../components/Form/AddCategories';
 import DashBoard from '../components/DashBoard';
 import Headers from '../Layouts/Headers';
+import fb from '../firebase'
+import Login from './Login';
+import { useNavigate } from 'react-router-dom';
 
 export interface IAddNewsPageProps {
 }
@@ -11,17 +14,37 @@ export interface IAddNewsPageProps {
 
 const AddNewsPage: FC<IAddNewsPageProps> = ({ }) => {
     const [isNewsPost, setIsNewPost] = useState(0)
+    const nav = useNavigate()
+
+    useEffect(() => {
+        const observer = fb.auth.onAuthStateChanged((user) => {
+            nav('/add-news', { replace: true })
 
 
 
 
-    // const { data, isLoading, error } = useSWR('fetchData', getPost)
+        })
+
+        return observer
+
+    }, [])
+
+    if (!fb.auth.currentUser) {
+        return <Login />
+    }
 
     return (
         <div className='flex flex-col  '>
             <Headers />
             <div className="h-20"></div>
+            <div className='flex items-center mx-6'>
 
+                <p> Hello {fb.auth.currentUser.email}</p>
+                <div className="w-4"></div>
+                <button className='bg-purple-500 p-2.5 rounded-md text-white shadow-md' onClick={() => fb.auth.signOut()}>Sign out</button>
+
+            </div>
+            <div className="h-8"></div>
             <div className="flex justify-around shadow-md">
                 <p className={isNewsPost === 0 ? 'border border-b-purple-500 p-3 bg-cyan-50 rounded-lg' : 'p-3'} onClick={() => setIsNewPost(0)}>Add News</p>
                 <p className={isNewsPost === 1 ? 'border border-b-purple-500 p-3 bg-cyan-50 rounded-lg' : 'p-3'} onClick={() => setIsNewPost(1)}>Add Categories</p>
---
+++ src/pages/Login.tsx
@@ -0,0 +1,76 @@
+import { FC, useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { signInWithEmailAndPassword } from 'firebase/auth'
+import fb from '../firebase'
+import { Alert, Spinner } from 'flowbite-react';
+export interface ILoginProps {
+}
+
+const Login: FC<ILoginProps> = ({ }) => {
+    const { register, handleSubmit, reset, formState: { errors } } = useForm()
+    const [isLoading, setLoading] = useState(false)
+    const [isError, setError] = useState("")
+
+    const onFormSubmit = (data: any) => {
+        const { email, password } = data;
+        setLoading(true)
+        signInWithEmailAndPassword(fb.auth, email, password).then((user) => {
+            console.log(user.user);
+        }).catch((e) => {
+            setError(e.message)
+        })
+        setLoading(false)
+        reset()
+
+
+    }
+
+
+
+    return (
+        <section className="bg-gray-50 dark:bg-gray-900">
+            <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+                    <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
+                    Progoti News Login
+                </div>
+                <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+                    <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+                        <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                            Sign in to your account
+                        </h1>
+                        <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit(onFormSubmit)}>
+                            <div>
+                                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+                                <input type="email" {...register('email')} className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
+                            </div>
+                            <div>
+                                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                                <input type="password" {...register("password")} placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+                            </div>
+                            <div className="flex items-center justify-between">
+                                <div className="flex items-start">
+                                    <div className="flex items-center h-5">
+                                        <input id="remember" aria-describedby="remember" type="checkbox" className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" />
+                                    </div>
+                                    <div className="ml-3 text-sm">
+                                        <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                                    </div>
+                                </div>
+                                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+                            </div>
+                            {isLoading && <Spinner />}
+                            <button disabled={isLoading} type="submit" className="w-full text-white bg-purple-600 hover:bg-purple-700 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-primary-800" >Sign in</button>
+                            <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                                Don’t have an account yet? <a href="#" className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</a>
+                            </p>
+                        </form>
+
+                        {isError && <Alert>{isError}</Alert>}
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+}
+export default Login
